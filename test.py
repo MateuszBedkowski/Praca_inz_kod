31a53804b7abd8a2b07886932087121337b67345
Mateusz Bedkowski <stoper.zaglebie@gmail.com>
2023-11-27 22:01:56 +0100

    working API call, returns number of CVEs, CVE ID and description

--- test.py
+++ test.py
@@ -8,20 +8,28 @@ def search_cve(keyword):
 
     if response.status_code == 200:
         data = response.json()
+        
         if 'vulnerabilities' in data:
             cve_items = data['vulnerabilities']
+            total_results = data['totalResults']
+            
+            print(f"Total vulnerabilities found: {total_results}\n")
+            
             for cve in cve_items:
                 cve_id = cve['cve']['id']
                 description = cve['cve']['descriptions'][0]['value']
+                
                 print(f"CVE ID: {cve_id}")
                 print(f"Description: {description}")
                 print("--------------------------------------------------------------\n")
+            
             if not cve_items:
                 print(f"No vulnerabilities found for {keyword}")
         else:
             print(f"No vulnerabilities found for {keyword}")
     elif response.status_code == 404:
         print(f"No CVE information found for {keyword}")
+    
     else:
         print(f"Error: {response.status_code}")
 
